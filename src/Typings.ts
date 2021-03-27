a6c73a04945fef427543db5ddbe0fb8f9cf52866
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2021-03-27 14:59:47 +0800

    feat: promp message resolvable

--- src/Typings.ts
+++ src/Typings.ts
@@ -1,4 +1,4 @@
-import { CategoryChannel, Message, MessageEmbed, PermissionString, Role, TextChannel, User, VoiceChannel } from 'discord.js';
+import { APIMessageContentResolvable, CategoryChannel, Message, MessageAdditions, MessageOptions, PermissionString, Role, TextChannel, User, VoiceChannel } from 'discord.js';
 import { Command } from '.';
 import { Disclosure } from './Disclosure';
 import { DiscordEvent } from './DiscordEvent';
@@ -112,7 +112,7 @@ export interface CommandDefinition extends ArgvDefinition {
 
 export type Prompts = Record<string, PromptOption>;
 
-export type MessageResolvable = any;
+export type MessageResolvable = APIMessageContentResolvable | (MessageOptions & { split?: false }) | MessageAdditions;
 
 export interface PromptOption {
     type: DataType;
