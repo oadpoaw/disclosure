2a038e1526528d96a925fad8f92c496deaa7c815
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2021-04-02 23:04:08 +0800

    feat: add create command script

--- src/scripts/disclosure.ts
+++ src/scripts/disclosure.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import minimist from 'minimist';
 import builder from 'minimist-options';
+import { createCommand } from './template/createCommand';
 import { project } from './util/project';
 
 const argv = minimist(process.argv.slice(2), builder({
@@ -19,6 +20,8 @@ const argv = minimist(process.argv.slice(2), builder({
         await project(argv.cwd);
 
     } else if (argv._[0] === 'command') {
+
+        await createCommand(argv);
 
     } else if (argv._[0] === 'event') {
 
