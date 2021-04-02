ff193b84fb072c2272c087535363076ac74bc7c3
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2021-04-02 23:21:26 +0800

    remove: ArgumentHandler from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { config } from 'dotenv';
 config();
 
-export * from './ArgumentHandler';
+export * from './database/util/validate';
 export * from './Command';
 export * from './Constants';
 export * from './Disclosure';
 export * from './DisclosureError';
 export * from './DiscordEvent';
+export * from './Dispatcher'
 export * from './Prompt';
 
 import { _scaffold } from './Scaffold';
