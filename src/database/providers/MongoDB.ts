63090284c55696448a1e862141600195a9750c00
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2021-04-04 16:32:12 +0800

    refactor/perf: Synchronous database providers

--- src/database/providers/MongoDB.ts
+++ src/database/providers/MongoDB.ts
@@ -2,46 +2,53 @@ import { ColumnType, ExtractColumnType, FunctionProvider, StoreProvider } from '
 import { DisclosureError } from '../../DisclosureError';
 import { validate } from '../util/validate';
 
-export async function MongoDB(uri: string): Promise<FunctionProvider> {
-    return new Promise((resolve, reject) => {
-        import('mongoose').then(async (mongoose) => {
-            try {
-                const connection = await mongoose.createConnection(uri, {
-                    useNewUrlParser: true,
-                    useUnifiedTopology: true,
-                    useFindAndModify: true,
-                });
-                const provider: FunctionProvider = <T extends ColumnType>(s: string, t: T) => {
-                    const type =
-                        t === 'boolean' ? Boolean :
-                            t === 'number' ? Number :
-                                t === 'string' ? String : undefined;
-                    const model = connection.model(s, new mongoose.Schema({
-                        key: String,
-                        value: type,
-                    }));
-                    return new class extends StoreProvider {
-                        async get(k: string): Promise<ExtractColumnType<T>> {
-                            const inst = await model.findOne({ key: k });
-                            if (inst) return inst.get('value');
-                            return undefined;
-                        }
-                        async set(k: string, v: ExtractColumnType<T>) {
-                            validate(v, t);
-                            const inst = await model.findOneAndUpdate({ key: k }, { value: v });
-                            if (!inst) await model.create({ key: k, value: v });
-                        }
-                        async del(k: string) {
-                            return !!(await model.deleteOne({ key: k })).deletedCount;
-                        }
-                    };
-                };
-                return resolve(provider);
-            } catch (err) {
-                reject(err);
-            }
-        }).catch(() => {
-            reject(new DisclosureError(`Please install \'mongoose\' manually.`));
+export function MongoDB(uri: string): FunctionProvider {
+    try {
+
+        const mongoose = require('mongoose') as typeof import('mongoose');
+
+        const connection = mongoose.createConnection(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: true,
         });
-    });
+
+        const provider: FunctionProvider = <T extends ColumnType>(s: string, t: T) => {
+
+            const type =
+                t === 'boolean' ? Boolean :
+                    t === 'number' ? Number :
+                        t === 'string' ? String : undefined;
+
+            const model = connection.model(s, new mongoose.Schema({
+                key: String,
+                value: type,
+            }));
+
+            return new class extends StoreProvider {
+
+                async get(k: string): Promise<ExtractColumnType<T>> {
+                    const inst = await model.findOne({ key: k });
+                    if (inst) return inst.get('value');
+                    return undefined;
+                }
+
+                async set(k: string, v: ExtractColumnType<T>) {
+                    validate(v, t);
+                    const inst = await model.findOneAndUpdate({ key: k }, { value: v });
+                    if (!inst) await model.create({ key: k, value: v });
+                }
+
+                async del(k: string) {
+                    return !!(await model.deleteOne({ key: k })).deletedCount;
+                }
+
+            };
+        };
+
+        return provider;
+
+    } catch (_err) {
+        throw new DisclosureError(`Please install \'mongoose\' manually.`);
+    }
 }
--- src/database/providers/Sequelize.ts
+++ src/database/providers/Sequelize.ts
@@ -2,46 +2,54 @@ import { ColumnType, ExtractColumnType, FunctionProvider, StoreProvider } from '
 import { DisclosureError } from '../../DisclosureError';
 import { validate } from '../util/validate';
 
-export async function Sequelize(uri: string): Promise<FunctionProvider> {
-    return new Promise((resolve, reject) => {
-        import('sequelize').then(async (sequelize) => {
-            try {
-                const connection = new sequelize.Sequelize(uri, { logging: false });
-                await connection.sync();
-                await connection.authenticate();
-                const provider: FunctionProvider = <T extends ColumnType>(s: string, t: T) => {
-                    const type =
-                        t === 'boolean' ? sequelize.DataTypes.BOOLEAN :
-                            t === 'number' ? sequelize.DataTypes.INTEGER :
-                                t === 'string' ? sequelize.DataTypes.STRING : undefined;
-                    const model = connection.define(s, {
-                        key: sequelize.DataTypes.STRING,
-                        value: type,
-                    }, { timestamps: false });
-                    model.sync();
-                    return new class extends StoreProvider {
-                        async get(k: string): Promise<ExtractColumnType<T>> {
-                            const inst = await model.findOne({ where: { key: k } });
-                            if (inst) return inst.getDataValue('value');
-                            return undefined;
-                        }
-                        async set(k: string, v: ExtractColumnType<T>) {
-                            validate(v, t);
-                            const inst = await model.findOne({ where: { key: k } });
-                            if (inst) await inst.update({ value: v }, { where: { key: v } });
-                            else await model.create({ key: k, value: v });
-                        }
-                        async del(k: string) {
-                            return !!(await model.destroy({ where: { key: k } }));
-                        }
-                    };
-                };
-                return resolve(provider);
-            } catch (err) {
-                reject(err);
-            }
-        }).catch(() => {
-            reject(new DisclosureError(`Please install \'sequelize\' manually.`));
-        });
-    });
+export function Sequelize(uri: string): FunctionProvider {
+    try {
+
+        const sequelize = require('sequelize') as typeof import('sequelize');
+
+        const connection = new sequelize.Sequelize(uri, { logging: false });
+
+        connection.sync().then(() => connection.authenticate()).catch(console.error);
+
+        const provider: FunctionProvider = <T extends ColumnType>(s: string, t: T) => {
+
+            const type =
+                t === 'boolean' ? sequelize.DataTypes.BOOLEAN :
+                    t === 'number' ? sequelize.DataTypes.INTEGER :
+                        t === 'string' ? sequelize.DataTypes.STRING : undefined;
+
+            const model = connection.define(s, {
+                key: sequelize.DataTypes.STRING,
+                value: type,
+            }, { timestamps: false });
+
+            model.sync();
+
+            return new class extends StoreProvider {
+
+                async get(k: string): Promise<ExtractColumnType<T>> {
+                    const inst = await model.findOne({ where: { key: k } });
+                    if (inst) return inst.getDataValue('value');
+                    return undefined;
+                }
+
+                async set(k: string, v: ExtractColumnType<T>) {
+                    validate(v, t);
+                    const inst = await model.findOne({ where: { key: k } });
+                    if (inst) await inst.update({ value: v }, { where: { key: v } });
+                    else await model.create({ key: k, value: v });
+                }
+
+                async del(k: string) {
+                    return !!(await model.destroy({ where: { key: k } }));
+                }
+
+            };
+        };
+
+        return provider;
+
+    } catch (_err) {
+        throw new DisclosureError(`Please install \'sequelize\' manually.`);
+    }
 }
