240894578819ff791b85ec5110c98816dde41bbc
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2021-04-07 18:21:50 +0800

    feat: interactive CLI

--- src/scripts/disclosure.ts
+++ src/scripts/disclosure.ts
@@ -1,16 +1,13 @@
 #!/usr/bin/env node
+
 import minimist from 'minimist';
 import builder from 'minimist-options';
+import { prompt } from 'inquirer';
 import { createCommand } from './template/createCommand';
 import createEvent from './template/createEvent';
-import { project } from './util/project';
+import { createProject } from './template/createProject';
 
 const argv = minimist(process.argv.slice(2), builder({
-    cwd: {
-        type: 'boolean',
-        default: false,
-        alias: ['c']
-    },
     force: {
         type: 'boolean',
         default: false,
@@ -25,17 +22,39 @@ const argv = minimist(process.argv.slice(2), builder({
         console.warn(`[WARN] Using '--force' or '-f'`);
     }
 
-    if (!argv._.length) {
-
-        await project(argv.cwd);
-
-    } else if (argv._[0] === 'command') {
+    if (argv._[0] === 'command') {
 
         await createCommand(argv);
 
     } else if (argv._[0] === 'event') {
 
         await createEvent();
+
+    } else {
+
+        const { answer } = await prompt([
+            {
+                type: 'list',
+                message: 'What do you want to create? (Ctrl+C to cancel)',
+                choices: ['Bot', 'Command', 'Event'],
+                name: 'answer',
+            },
+        ]) as { answer: 'Bot' | 'Command' | 'Event'; };
+
+        switch (answer) {
+            case 'Bot': {
+                createProject();
+                break;
+            }
+            case 'Command': {
+                createCommand(argv);
+                break;
+            }
+            case 'Event': {
+                createEvent();
+                break;
+            }
+        }
 
     }
 
